4aa9e12a9da0c6b863d60bf3f5bec0021c31e9af
AndyLinxies <linxies@gmail.com>
2021-04-21 12:24:20 +0200

    Var tempo exo

---
+++ public/js/main.js
@@ -0,0 +1,81 @@
+//let a = 1; 
+//let b = 2;
+//let c = 3;
+
+//let temp;
+//temp = a;
+//a=b;
+//b=c;
+//c=temp;
+//console.log(a,b,c);
+//Assigner d'abord une valeur a temp : temp=.
+//Savoir que b=c ne veut pas dire que  c=b ! toujours se soucier du côté gauche de l'égalité.
+
+//Exo1
+//let temp1e;
+//let temp2e;
+//let a = 1;
+//let b = 2;
+//let c = 3;
+//let d = 4;
+
+//temp1e=b; //temp1=2
+//temp2e=a;//temp2e=1
+//L'utilisation de deux valeurs temporelles depend de l'ordre des variables
+//a=d;//a=4
+//b=c;//b=3
+//c=temp1e//c=2
+//d=temp2e//d=1
+
+//console.log(a,b,c,d);
+
+//Exo2
+//let a = 1; 
+//let b = 2;
+//let c = 3;
+//let d = 4;
+
+//let tempo;
+//tempo=a;//tempo=1
+//a=b;//a=2
+//b=c;//b=3
+//c=d;//c=4
+//d=tempo;
+
+//console.log(a,b,c,d);
+
+//Exo3
+//let tableau = [1, 2, 3, 4];
+//temp=tableau[0];
+//tableau[0]=tableau[3]
+
+//Exo4
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4; 
+let e = 5;
+let f = 6;
+
+//let tempo1;
+//let tempo2;
+//let tempo3;
+
+//tempo1=a;//tempo1=1
+//tempo2=b;//tempo2=2
+//tempo3=c;//tempo3=3
+//a=d;//a=4
+//b=e;//b=5
+//c=tempo1;//c=1
+//d=tempo2;//d=2
+//e=f;//e=6
+//f=tempo3//f=3
+
+//console.log(a,b,c,d,e,f);
+
+let tempo1;
+
+
+
+
+
